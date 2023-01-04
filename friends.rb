be9b76400bc32ec1836fb4b3b8c2383005568927
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-01-04 17:44:07 -0500

    add places and pop

--- friends.rb
+++ friends.rb
@@ -3,3 +3,6 @@ my_friends = ["Bill", "Joe", "Henry", "Travis", "Josh"]
 puts my_friends[3]
 puts my_friends[4]
 puts my_friends.first
+
+places = ["Argentina", "France", "Canada", "Japan", "Germany"]
+puts places.pop
